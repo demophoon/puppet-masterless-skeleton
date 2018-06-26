d8d1f40706e54110328ac54e3ba275de418e0235
Britt Gresham <britt@brittg.com>
2018-06-26 16:46:27 -0700

    Correctly use params.pp to lookup variables.

--- manifests/masterless.pp
+++ manifests/masterless.pp
@@ -2,21 +2,25 @@
 # its own in a masterless environment.
 class profiles::masterless (
   $default_hiera_sources = {},
-  $run_cron              = {
-    'minute' => ['0', '30'],
-  },
+  $control_repo_uri      = $profiles::params::control_repo_uri,
+  $run_cron              = { 'minute' => ['0', '30'] },
 ) inherits profiles::params {
+
+  # Puppet apply settings
   $puppet_bin_dir = "/opt/puppetlabs/puppet/bin"
   $puppet_code_dir = '/etc/puppetlabs/code'
+  $environment_path = "${puppet_code_dir}/environments/${::environment}"
+  $module_path = "${environment_path}/modules/"
+  $site_pp = "${environment_path}/site.pp"
 
+  # r10k sources setup
   $default_r10k_sources = {
     'puppet' => {
-      'remote' => $::params::control_repo_uri,
+      'remote' => $control_repo_uri,
       'basedir'=> "${puppet_code_dir}/environments",
       'prefix' => false,
     }
   }
-
   $r10k_source_defaults = deep_merge($default_r10k_sources, $default_hiera_sources)
   $r10k_sources = deep_merge($r10k_source_defaults, $additional_r10k_sources)
 
@@ -24,12 +28,6 @@ class profiles::masterless (
     sources    => $r10k_sources,
     modulepath => "${::settings::confdir}/environments/\$environment/modules:/opt/puppet/share/puppet/modules",
   }
-
-  $environment_path = "${puppet_code_dir}/environments/${::environment}"
-
-  $module_path = "${environment_path}/modules/"
-  $site_pp = "${environment_path}/site.pp"
-
   cron { 'Update r10k and run puppet apply':
     command => "${puppet_bin_dir}/r10k deploy environment -pv || ${puppet_bin_dir}/puppet apply --modulepath ${module_path} ${site_pp}",
     user    => 'root',
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,4 @@
 class profiles::params {
+  ## Update the url below to point to your control repo!
   $control_repo_uri = 'https://github.com/demophoon/puppet-masterless-skeleton.git'
 }
