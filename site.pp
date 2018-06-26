3452d50cf8107bb7acd9e21aac5d6613f09c2d5c
Britt Gresham <britt@brittg.com>
2018-06-26 16:35:06 -0700

    Add notes to site.pp regarding default classification and fix warning.

--- site.pp
+++ site.pp
@@ -1,5 +1,14 @@
+################################################################################
+## Do not delete the following line. It includes the important masterless secret sauce.
+################################################################################
+include profiles::masterless
+
+
+################################################################################
+## Add custom classification below
+################################################################################
 node default {
   notify {'Please add classification to /etc/puppetlabs/code/environments/production/site.pp':
-    loglevel => warn,
+    loglevel => warning,
   }
 }
